6a5d3899a82d15748f22e1f54820f4cb3ada04cf
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-20 11:12:13 +0300

    this commit adds the contact form to the repo

--- contact.js
+++ contact.js
@@ -1,16 +1,25 @@
 const form = document.querySelector('form')
 
+const url = 'http://localhost:3000/contacts'
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
-})
+    console.log('hello')
 
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData)
 
-fetch('http://localhost:3000/contacts', {
-    method : 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
+    console.log(data)
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+    .catch(err=>console.log(err))
+
 })
-.then()
